d180135618ef212bf236c43c5bfe12199a288af6
ankit <69717009+crew-guy@users.noreply.github.com>
2021-05-30 01:20:31 +0530

    subscribed to singleton pattern

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+// Enum for the status of a project
+enum ProjectStatus{
+    Active = 'active',
+    Finished = 'finished'
+}
+
+// Project class
+class Project {
+    constructor(
+        public id: string,
+        public title:string,
+        public description:string,
+        public people:number,
+        public status:ProjectStatus,
+    ) {
+        
+    }
+}
+
 // Validation interface
 interface Validatable {
     value: string | number,
@@ -42,21 +61,84 @@ const validate = (validatableInput: Validatable) => {
     return isValid
 }
 
+
+type Listener = (projects:Project[]) => void
+
+class ProjectState {
+    private listeners:Listener[] = []
+    private projects: Project[] = []
+    private static instance:ProjectState
+
+    private constructor() {
+        
+    }
+
+    static getInstance() {
+        if (this.instance)
+            return this.instance
+        this.instance = new ProjectState()
+        return this.instance
+    }
+
+    addListeners(listenerFn: Listener) {
+        this.listeners.push(listenerFn)
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+        const newProject = new Project(
+            Math.random().toString(),
+            title,
+            description,
+            numOfPeople,
+            ProjectStatus.Active
+        )
+        this.projects.push(newProject)
+        for (const listenerFn of this.listeners) {
+            // Pass to this listener function a copy of the state that this class is managing i.e. the "projects" list
+            // We call "slice" to return a copy of the array and not the original "array" to make the "projects" list to :
+            // 1. make the state uneditable from the place the listener function is coming from
+            // 2. ensure that if we push something to it from inside, state would change everywhere else in the app but these places would not really notice that had changed
+            listenerFn(this.projects.slice())
+        }
+    }
+
+}
+
+const projectState = ProjectState.getInstance()
+
 class ProjectList {
     hostElement: HTMLDivElement;
     templateElement: HTMLTemplateElement;
     element: HTMLElement;
+    assignedProjects : Project[]
 
-    constructor(private type : 'active' | 'finished') {
+    constructor(private type : ProjectStatus) {
         this.hostElement = document.querySelector('#app')! as HTMLDivElement;
         this.templateElement = document.querySelector('#project-list')! as HTMLTemplateElement;
-        
+        this.assignedProjects = []
+
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`
+        
+        projectState.addListeners((projects:Project[]) => {
+            this.assignedProjects = projects
+            this.renderProjects()
+        })
+        
         this.attach()
         this.renderContent()
     }
+    private renderProjects() {
+        const listEl = document.querySelector(`#${this.type}-projects-list`)! as HTMLUListElement;
+        for (const projectItem of this.assignedProjects) {
+            const listItem = document.createElement('li');
+            listItem.textContent = projectItem.title
+            listEl.appendChild(listItem)
+        }
+    }
+
+
     private renderContent() {
         const listId = `${this.type}-projects-list`
         this.element.querySelector('ul')!.id = listId
@@ -161,7 +243,7 @@ class ProjectInput {
         const userInput = this.gatherUserInput()
         if (Array.isArray(userInput)) {
             const [title, description, people] = userInput
-            console.log(title, description, people)
+            projectState.addProject(title, description, people)
             this.clearInput()
         }
     }
@@ -175,35 +257,6 @@ class ProjectInput {
     }
 }
 
-class ProjectState {
-    private projects: any[] = []
-    private static instance:ProjectState
-
-    private constructor() {
-        
-    }
-
-    static getInstance() {
-        if (this.instance)
-            return this.instance
-        this.instance = new ProjectState()
-        return this.instance
-    }
-
-
-    addProject(title: string, description: string, numOfPeople: number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title,
-            description,
-            people:numOfPeople
-        }
-        this.projects.push(newProject)
-    }
-
-}
-
 const prjInput = new ProjectInput()
-const activePrjList = new ProjectList('active')
-const finishedPrjList = new ProjectList('finished')
-const projectState = ProjectState.getInstance()
+const activePrjList = new ProjectList(ProjectStatus.Active)
+const finishedPrjList = new ProjectList(ProjectStatus.Finished)
